53c60696bd715ebcfaec369fe3a2b10487deab7c
Sasha Murzin <murzin@gmail.com>
2020-02-14 02:40:19 +0200

    Var names and comments

--- fcalc.pl
+++ fcalc.pl
@@ -1,15 +1,17 @@
 #!/usr/bin/env perl
 
-# NOTE: just for sure: NO SUCH VAR NAMES IN REAL PROD, this script was done just for fun
+# see https://en.wikipedia.org/wiki/Reverse_Polish_notation
+# based on https://habr.com/ru/post/100869/ (russian)
 
 use v5.26.0;
 use warnings;
 no warnings 'recursion';
 
-my (@ms, @ky, %dm, %ops, @pr, $again);
-my @th = qw[| + - * / ( )];
-my %sy = map {$_ => 1} @th;
-my %mx = (
+my (@temp_stack, @main_stack, %work_mx, %ops, @primary_stack, $again);
+
+my @mx_headrow = 
+            qw[| + - * / ( )];
+my %matrix = (
     '|' => [qw[4 1 1 1 1 1 5]],
     '+' => [qw[2 2 2 1 1 1 2]],
     '-' => [qw[2 2 2 1 1 1 2]],
@@ -17,95 +19,115 @@ my %mx = (
     '/' => [qw[2 2 2 2 2 1 2]],
     '(' => [qw[5 1 1 1 1 1 3]],
 );
+my %controls = map {$_ => 1} @mx_headrow;
+
 for my $i (0..6) {
-    for (@th) {
-        next unless exists $mx{$_};
-        $dm{$th[$i].$_} = $mx{$_}->[$i];
+    for (@mx_headrow) {
+        $work_mx{$mx_headrow[$i].$_} = $matrix{$_}->[$i];
     }
 }
 
+# calculations
+# receiving two arrayrefs representing fractions
+# doing apropriate math with fractions
+# returning normalized arrayref as result
 @ops{qw[+ - * /]} = (
     sub { 
-        my $nok = nok($_[0]->[1], $_[1]->[1]);
-        my $c = $_[0]->[0] * ($nok / $_[0]->[1]) + $_[1]->[0] * ($nok / $_[1]->[1]);
-        my $nod = nod($c, $nok);
-        return [$c/$nod, $nok/$nod];
+        my $lcm = lcm($_[0]->[1], $_[1]->[1]);
+        my $c = $_[0]->[0] * ($lcm / $_[0]->[1]) + $_[1]->[0] * ($lcm / $_[1]->[1]);
+        my $gcd = gcd($c, $lcm);
+        return [$c/$gcd, $lcm/$gcd];
     },
     sub { 
-        my $nok = nok($_[0]->[1], $_[1]->[1]);
-        my $c = $_[1]->[0] * ($nok / $_[1]->[1]) - $_[0]->[0] * ($nok / $_[0]->[1]);
-        my $nod = nod($c, $nok);
-        return [$c/$nod, $nok/$nod];
+        my $lcm = lcm($_[0]->[1], $_[1]->[1]);
+        my $c = $_[1]->[0] * ($lcm / $_[1]->[1]) - $_[0]->[0] * ($lcm / $_[0]->[1]);
+        my $gcd = gcd($c, $lcm);
+        return [$c/$gcd, $lcm/$gcd];
     },
     sub {
         my $c = $_[0]->[0] * $_[1]->[0];
         my $z = $_[0]->[1] * $_[1]->[1];
-        my $nod = nod($c, $z);
-        return [$c/$nod, $z/$nod];
+        my $gcd = gcd($c, $z);
+        return [$c/$gcd, $z/$gcd];
     },
     sub {
         my $c = $_[1]->[0] * $_[0]->[1];
         my $z = $_[1]->[1] * $_[0]->[0];
-        my $nod = nod($c, $z);
-        return [$c/$nod, $z/$nod];
+        my $gcd = gcd($c, $z);
+        return [$c/$gcd, $z/$gcd];
     },
 );
 
-sub nok {
+# Least Common Multiple
+sub lcm {
     my ($x, $y) = @_;
-    return $x * $y / nod($x, $y);
+    return $x * $y / gcd($x, $y);
 }
 
-sub nod {
+# Greatest Common Divisor
+sub gcd {
     my ($x, $y) = map {abs} @_;
     return $x if $x == $y;
     return $y unless $x;
-    return ($x >= $y) ? nod($x-$y, $y) : nod($x, $y-$x);
+    return ($x >= $y) ? gcd($x-$y, $y) : gcd($x, $y-$x);
 }
+
+# main loop
+
 say "Fractional calculator";
-say "fraction should be presented as `x:y`, operations: + - * /, for negation do like (0 - x:y)";
+say "Fraction should be presented as `x:y`, operations: + - * /, for negation do like (0 - x:y)";
+say "Example: (1:3 - 7:4) * 2:3 - (5:7 + 1:3) / 3:5";
 
 while (1) {
     print '> ';
-    my $in = <>;
+    my $in = <>; # get the input string
     
-    $in    =~ s/\s*:\s*/:/g;
-    $in    =~ s/([()+*\/-])/ $1 /g;
-    my @in =  map  { s/^0+(\d+):/$1:/r }
+    $in    =~ s/\s*:\s*/:/g;                # normalizing input string:
+    $in    =~ s/([()+*\/-])/ $1 /g;         # removing spaces around :, adding spaces around ops
+    my @in =  map  { s/^0+(\d+):/$1:/r }    # parsing string into array
               map  { s/:0+$/:0/r }
               map  { /^\d+$/ ? $_.':1' : $_ }
-              grep { length $_ }
+              grep { length }
               split /\s+/, $in;
     @in or next;
-    undef $again;
+    $again = 0; # if up - bad input, start main loop again
     for (@in) {
-        unless(/^\d+:\d+$/ and $_ !~ /:0$/ or $sy{$_}) {
-            say "no such shit > $_ < in my cozy garden!";
-            $again = 'yea'; last;
+        unless(/^\d+:\d+$/ and $_ !~ /:0$/ or $controls{$_}) { # check for allowed tokens
+            say "unknown token: $_";
+            $again = 1; last;
         }
     }
     next if $again;
+    # so @in contains allowed operations and fractions in x:y form
+    # transform x:y str to arrayref [x, y] - prepare for calculations
     for (0..$#in) {
         $in[$_] = [split ':', $in[$_]] if $in[$_] =~ /:/;
     }
     
-    @ms = qw(|);
-    @ky = @pr = qw();
+    # reorganizing @in to polish notation
+    # based om article algorithm
+    @temp_stack = qw(|);
+    @main_stack = @primary_stack = qw();
     in: for my $t (@in, '|') {
-        if ($sy{$t}) {
-            for ($dm{$t.$ms[$#ms]}) {
-                /1/ && do { push @ms, $t };
-                /2/ && do { push @ky, pop @ms; redo in };
-                /3/ && do { pop @ms };
-                /5/ && do { say "wrong input $in"; $again = 'yea'; last in };
+        if ($controls{$t}) {
+            for ($work_mx{$t.$temp_stack[$#temp_stack]}) { # just switch
+                /1/ && do { push @temp_stack, $t };
+                /2/ && do { push @main_stack, pop @temp_stack; redo in };
+                /3/ && do { pop  @temp_stack };
+                /5/ && do { say "wrong input $in"; $again = 1; last in };
             }
         } else {
-            push @ky, $t;
+            push @main_stack, $t;
         }
     }
     next if $again;
     
-    $sy{$_} ? push @pr, $ops{$_}->(pop @pr, pop @pr) : push @pr, $_ for @ky;
+    # main calc loop - populating primary stack
+    $controls{$_} ? push @primary_stack, $ops{$_}->(pop @primary_stack, pop @primary_stack) 
+                  : push @primary_stack, $_ 
+                      for @main_stack;
     
-    say $pr[0]->[0] . ($pr[0]->[1] != 1 && ':'.$pr[0]->[1]);
+    # @primary_stack contains 1 element - result
+    # output result converting x:1 => x
+    say $primary_stack[0]->[0] . ($primary_stack[0]->[1] != 1 && ':'.$primary_stack[0]->[1]);
 }
